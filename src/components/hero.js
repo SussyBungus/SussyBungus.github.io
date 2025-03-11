a0fdfec5b12c0d8d875bec653ffa9104d8607603
raymond <toronto4raymond2010@gmai.com>
2025-03-10 22:47:27 -0400

    Update message

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,17 +1,20 @@
 import React from "react";
+import amoung from "../assets/amoung.png";
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
-        <h1 className="hero__title">Welcome Sussies</h1>
-        <p className="hero__description">asd</p>
+        <h1 className="hero__title">Welcome to the Sussy Zone!</h1>
+        <p className="hero__description">
+          The ultimate destination for all things sus. Join the fun and embrace the chaos!
+        </p>
         <a href="#products" className="hero__cta">
-          Click
+          Join the Sussies
         </a>
       </div>
       <div className="hero__image">
-        <img src="/amoung.png" alt="Hero" />
+        <img src={amoung} alt="Hero" />
       </div>
     </section>
   );
